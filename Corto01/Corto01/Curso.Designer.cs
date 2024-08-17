c4cd7c1b37012e437c9fa30accc38f86fb5e50ab
Flores-Oz <oscrecreate@gmail.com>
2024-08-16 20:40:45 -0600

    Avance

--- Corto01/Corto01/Curso.Designer.cs
+++ Corto01/Corto01/Curso.Designer.cs
@@ -33,6 +33,23 @@
             this.label2 = new System.Windows.Forms.Label();
             this.comboBoxCurso = new System.Windows.Forms.ComboBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxidAlumCurso = new System.Windows.Forms.TextBox();
+            this.textBoxCarne = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxCurso = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxFinal = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxZona = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxTotal = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxLiteral = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.buttonActualizar = new System.Windows.Forms.Button();
+            this.comboBoxArgumento = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -79,14 +96,177 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(475, 426);
+            this.dataGridView1.Size = new System.Drawing.Size(708, 426);
             this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentDoubleClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "id_Alum_Curso";
+            // 
+            // textBoxidAlumCurso
+            // 
+            this.textBoxidAlumCurso.Enabled = false;
+            this.textBoxidAlumCurso.Location = new System.Drawing.Point(139, 119);
+            this.textBoxidAlumCurso.Name = "textBoxidAlumCurso";
+            this.textBoxidAlumCurso.Size = new System.Drawing.Size(142, 22);
+            this.textBoxidAlumCurso.TabIndex = 6;
+            // 
+            // textBoxCarne
+            // 
+            this.textBoxCarne.Enabled = false;
+            this.textBoxCarne.Location = new System.Drawing.Point(139, 147);
+            this.textBoxCarne.Name = "textBoxCarne";
+            this.textBoxCarne.Size = new System.Drawing.Size(142, 22);
+            this.textBoxCarne.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Carne";
+            // 
+            // textBoxCurso
+            // 
+            this.textBoxCurso.Enabled = false;
+            this.textBoxCurso.Location = new System.Drawing.Point(139, 174);
+            this.textBoxCurso.Name = "textBoxCurso";
+            this.textBoxCurso.Size = new System.Drawing.Size(142, 22);
+            this.textBoxCurso.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Curso";
+            // 
+            // textBoxFinal
+            // 
+            this.textBoxFinal.Location = new System.Drawing.Point(139, 228);
+            this.textBoxFinal.Name = "textBoxFinal";
+            this.textBoxFinal.Size = new System.Drawing.Size(142, 22);
+            this.textBoxFinal.TabIndex = 14;
+            this.textBoxFinal.TextChanged += new System.EventHandler(this.textBoxFinal_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Final";
+            // 
+            // textBoxZona
+            // 
+            this.textBoxZona.Location = new System.Drawing.Point(139, 201);
+            this.textBoxZona.Name = "textBoxZona";
+            this.textBoxZona.Size = new System.Drawing.Size(142, 22);
+            this.textBoxZona.TabIndex = 12;
+            this.textBoxZona.TextChanged += new System.EventHandler(this.textBoxZona_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Zona";
+            // 
+            // textBoxTotal
+            // 
+            this.textBoxTotal.Location = new System.Drawing.Point(139, 257);
+            this.textBoxTotal.Name = "textBoxTotal";
+            this.textBoxTotal.Size = new System.Drawing.Size(142, 22);
+            this.textBoxTotal.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(24, 260);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Total";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(24, 320);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 16);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Argumento";
+            // 
+            // textBoxLiteral
+            // 
+            this.textBoxLiteral.Location = new System.Drawing.Point(139, 288);
+            this.textBoxLiteral.Name = "textBoxLiteral";
+            this.textBoxLiteral.Size = new System.Drawing.Size(142, 22);
+            this.textBoxLiteral.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(24, 291);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(43, 16);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Literal";
+            // 
+            // buttonActualizar
+            // 
+            this.buttonActualizar.Location = new System.Drawing.Point(76, 364);
+            this.buttonActualizar.Name = "buttonActualizar";
+            this.buttonActualizar.Size = new System.Drawing.Size(163, 62);
+            this.buttonActualizar.TabIndex = 21;
+            this.buttonActualizar.Text = "Actualizar";
+            this.buttonActualizar.UseVisualStyleBackColor = true;
+            this.buttonActualizar.Click += new System.EventHandler(this.buttonActualizar_Click);
+            // 
+            // comboBoxArgumento
+            // 
+            this.comboBoxArgumento.FormattingEnabled = true;
+            this.comboBoxArgumento.Location = new System.Drawing.Point(139, 317);
+            this.comboBoxArgumento.Name = "comboBoxArgumento";
+            this.comboBoxArgumento.Size = new System.Drawing.Size(142, 24);
+            this.comboBoxArgumento.TabIndex = 22;
             // 
             // Curso
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1033, 450);
+            this.Controls.Add(this.comboBoxArgumento);
+            this.Controls.Add(this.buttonActualizar);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxLiteral);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxTotal);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxFinal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxZona);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxCurso);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxCarne);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxidAlumCurso);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBoxCurso);
@@ -108,5 +288,22 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBoxCurso;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxidAlumCurso;
+        private System.Windows.Forms.TextBox textBoxCarne;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxCurso;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxFinal;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxZona;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxTotal;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxLiteral;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button buttonActualizar;
+        private System.Windows.Forms.ComboBox comboBoxArgumento;
     }
 }
--- Corto01/Corto01/Curso.cs
+++ Corto01/Corto01/Curso.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Corto01
 {
@@ -41,6 +43,12 @@ namespace Corto01
             comboBoxSemestre.DataSource = semest;
             comboBoxSemestre.DisplayMember = "Nombre_Semestre";
             comboBoxSemestre.ValueMember = "Id_Semestre";
+
+            // Assuming comboBox1 is your ComboBox
+            comboBoxArgumento.Items.Add("Aprobado");
+            comboBoxArgumento.Items.Add("Reprobado");
+            comboBoxArgumento.Items.Add("Sin Derecho a Examen");
+            comboBoxArgumento.Items.Add("No se Presento");
         }
 
         private void comboBoxCurso_SelectedIndexChanged(object sender, EventArgs e)
@@ -50,10 +58,90 @@ namespace Corto01
                 int cod = Convert.ToInt16(comboBoxCurso.SelectedValue);
                 var signal = from sm in milinq.Asigna_Curso
                            where Convert.ToInt16(sm.Id_Curso) == cod
-                           select sm;
+                           select new { 
+                               ID_Alum = sm.id_alum_curso,
+                               Carne = sm.Carne,
+                               id_Curso = sm.Id_Curso,
+                               Zona = sm.Zona,
+                               Final = sm.Final,
+                               Total = sm.Final,
+                               Literal = sm.Literal,
+                               Argumento = sm.Argumento,
+                               Fecha_Asignacion = sm.fecha_asignacion,
+                               Fecha_Ingreso_Nota = sm.fecha_ingreso_nota
+                };
                 dataGridView1.DataSource = signal.ToList();
             }
             catch (Exception ex) { }
+        }
+
+        private void textBoxZona_TextChanged(object sender, EventArgs e)
+        {
+            string pattern = @"^(?:[0-5]?[0-9]|60)$";
+            if (Regex.IsMatch(textBoxZona.Text, pattern))
+            {
+                int Z = Convert.ToInt16(textBoxZona.Text);
+                if(Z == 0)
+                {
+                    textBoxLiteral.Text = "NSP";
+                }else if(Z <= 30){
+                    textBoxLiteral.Text = "SDE";
+                }
+            }
+            else
+            {
+                // La entrada no es válida
+                MessageBox.Show("Por favor, introduce un número entre 0 y 60.");
+                textBoxZona.Clear(); // Limpia el TextBox si la entrada no es válida
+            }
+        }
+
+        private void textBoxFinal_TextChanged(object sender, EventArgs e)
+        {
+            string pattern = @"^(?:[0-3]?[0-9]|40)$";
+            if (Regex.IsMatch(textBoxFinal.Text, pattern))
+            {
+                textBoxTotal.Text = (Convert.ToInt16(textBoxFinal.Text) + Convert.ToInt16(textBoxZona.Text)).ToString();
+            }
+            else
+            {
+                // La entrada no es válida
+                MessageBox.Show("Por favor, introduce un número entre 0 y 40.");
+                textBoxFinal.Clear(); // Limpia el TextBox si la entrada no es válida
+            }
+        }
+
+        private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+         
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1)
+            {
+                textBoxidAlumCurso.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                textBoxCarne.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+                textBoxCurso.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            }
+        }
+
+        private void buttonActualizar_Click(object sender, EventArgs e)
+        {
+            var buscar = from asig in milinq.Asigna_Curso
+                         where asig.id_alum_curso == Convert.ToInt16(textBoxidAlumCurso.Text)
+                         select asig;
+            foreach (var edita in buscar)
+            {
+                edita.Zona = Convert.ToInt16(textBoxZona.Text);
+                edita.Final = Convert.ToInt16(textBoxFinal.Text);
+                edita.Total = Convert.ToInt16(textBoxTotal.Text);
+                edita.Literal = textBoxLiteral.Text;
+                edita.Argumento = comboBoxArgumento.SelectedItem.ToString();
+            }
+            milinq.SubmitChanges();
+            MessageBox.Show("Editado Correctamente");
+
         }
     }
 }
